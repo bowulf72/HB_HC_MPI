8d2fccf3089154082e10bb0611d4c7156b8f30d7
Steven Roach <stevenwroach@hotmail.com>
2019-03-15 20:50:15 -0700

    Update loaddrivers.sh

--- loaddrivers.sh
+++ loaddrivers.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-
 set -x
 
+#disable_selinux
+sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
+setenforce 0
+
+yum -y install git
+
+git clone https://github.com/jithinjosepkl/azhpc-images.git
+cd azhpc-images/centos-7.6-hpc
+
+./setup_node.sh
